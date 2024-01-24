102808dd157a9c294dff8420baeb5e95b727c069
Swer4412 <swer4412@gmail.com>
2024-01-24 22:48:24 +0100

    Bella

---
+++ cmd/tutorial1/main.go
@@ -0,0 +1,6 @@
+package main //Serve per indicare al compilatore che dentro questo package (cartella) c'è una funzione main da cui partire
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
