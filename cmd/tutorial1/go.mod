d75fa0c23dd541297e79d02cc0a8b1b75f5da9c5
Swer4412 <swer4412@gmail.com>
2024-01-26 13:03:04 +0100

    15.24

---
+++ cmd/tutorial1/go.mod
@@ -0,0 +1,3 @@
+module boh
+
+go 1.21.6
--- cmd/tutorial1/main.go
+++ cmd/tutorial1/main.go
@@ -1,6 +1,11 @@
 package main //Serve per indicare al compilatore che dentro questo package (cartella) c'è una funzione main da cui partire
-import "fmt"
+import ("fmt"
+"unicode/utf8")
 
 func main() {
-	fmt.Println("Hello world!")
+	foo()
+}
+
+func foo(numero int) (int, int) {
+	return numero*2, numero/2
 }
