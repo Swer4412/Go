26b1b415fb061f83dada968f37528afc6211ece4
Swer4412 <swer4412@gmail.com>
2024-01-27 23:33:21 +0100

    43.56

--- cmd/tutorial1/main.go
+++ cmd/tutorial1/main.go
@@ -1,19 +1,30 @@
 package main //Serve per indicare al compilatore che dentro questo package (cartella) c'è una funzione main da cui partire
 import ("fmt"
+"time"
+"sync"
 )
+var m = sync.Mutex{}
+var wg= sync.WaitGroup{} //Nuovo waitgroup
+var results = []string{}
+var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 
 func main() {
-	var thing1 = [5]float64{1,2,3,4,5}
-	fmt.Printf("\nMemory location of thing1 is: %p", &thing1)
-	var result [5]float64 = square(&thing1) //Passo indirizzo di memoria
-	fmt.Printf("\nThe result is : %v", result)
-	fmt.Printf("\nThe value of thing1 is : %v", thing1)
+	t0 := time.Now()
+	for i:=0; i<len(dbData); i++ {
+		wg.Add(1) //aumento di uno il numero di task
+		go dbCall(i)
+	}
+	wg.Wait() //Aspetta che tutte le task eseguano Done()
+	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
+	fmt.Printf("\nThe results are %v", results)
 }
 
-func square(thing2 *[5]float64) [5]float64{
-	fmt.Printf("\nMemory location of thing2 is: %p", thing2)
-	for i := range *thing2 {
-		thing2[i] = thing2[i]*thing2[i]
-	}
-	return *thing2
+func dbCall(i int) {
+	var delay float32 = 2000 //Tolgo randomizzazione
+	time.Sleep(time.Duration(delay)*time.Millisecond)
+	fmt.Println("The reuslt from the database:", dbData[i])
+	m.Lock() //Guarda se la risorsa è già stata presa da un altra task
+	results = append(results, dbData[i])
+	m.Unlock() //Una volta acceduto alla risorsa, la si sblocca 
+	wg.Done()
 }
