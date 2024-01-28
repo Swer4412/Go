70d4b2daffa8d08824a73b5b58bb6e4ab6b44c18
Swer4412 <swer4412@gmail.com>
2024-01-28 22:56:42 +0100

    55.46

--- cmd/tutorial1/main.go
+++ cmd/tutorial1/main.go
@@ -1,30 +1,39 @@
 package main //Serve per indicare al compilatore che dentro questo package (cartella) c'è una funzione main da cui partire
-import ("fmt"
-"time"
-"sync"
+import (
+	"fmt"
 )
-var m = sync.Mutex{}
-var wg= sync.WaitGroup{} //Nuovo waitgroup
-var results = []string{}
-var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 
-func main() {
-	t0 := time.Now()
-	for i:=0; i<len(dbData); i++ {
-		wg.Add(1) //aumento di uno il numero di task
-		go dbCall(i)
-	}
-	wg.Wait() //Aspetta che tutte le task eseguano Done()
-	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
-	fmt.Printf("\nThe results are %v", results)
+type gasEngine struct {
+    mpg uint8
+    gallons uint8
 }
 
-func dbCall(i int) {
-	var delay float32 = 2000 //Tolgo randomizzazione
-	time.Sleep(time.Duration(delay)*time.Millisecond)
-	fmt.Println("The reuslt from the database:", dbData[i])
-	m.Lock() //Guarda se la risorsa è già stata presa da un altra task
-	results = append(results, dbData[i])
-	m.Unlock() //Una volta acceduto alla risorsa, la si sblocca 
-	wg.Done()
+type electricEngine struct{
+    mpkwh uint8
+    kwh uint8
+}
+
+type car [T gasEngine | electricEngine] struct{
+	carMake string
+	carModel string
+	engine T
+}
+
+func main() {
+	var gasCar = car[gasEngine]{
+		carMake: "Honda",
+		carModel: "Civic",
+		engine: gasEngine{
+			gallons:12.4,
+			mpg:40
+		}
+	}
+	var electricCar = car[electricEngine]{
+		carMake: "Tesla",
+		carModel: "Model 3",
+		engine: electricEngine{
+			kwh:57.5,
+			mpg:4.17
+		}
+	}
 }
